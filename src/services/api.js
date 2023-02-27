1f4a4178e868ad4d552571bb8f01d325228703a6
masbagaspn <masbagaspn@gmail.com>
2023-02-27 10:35:49 +0700

    fix: api services bug

--- src/services/api.js
+++ src/services/api.js
@@ -3,11 +3,11 @@ import { api } from "../utils/http-utils"
 const API_KEY_QUERY = `apiKey=${import.meta.env.VITE_API_KEY}`
 
 const getRandomRecipes = (query) => {
-    return api.get(`/recipes/random?${query}&number=10&${API_KEY_QUERY}`)
+    return api.get(`/recipes/random?${API_KEY_QUERY}&number=10${query ? `&${query}` : ''}`)
 }
 
 const getRecipesByQuery = (query) => {
-    return api.get(`/recipes/complexSearch?number=12&${query}&${API_KEY_QUERY}`)
+    return api.get(`/recipes/complexSearch?${API_KEY_QUERY}&number=12&${query}`)
 }
 
 const getRecipeInformationById = (id) => {
